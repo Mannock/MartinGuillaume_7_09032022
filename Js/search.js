381eb7987bc70729aeda2e700ab7bf1439813240
Martin Guillaume <pour-le-travail@hotmail.fr>
2022-06-01 11:03:33 +0200

    cleanup algo2

--- Js/search.js
+++ Js/search.js
@@ -1,27 +1,6 @@
 // ---------------------  MAIN SEARCH
 
 import { allRecipes } from "./script.js";
-// import { tagsIngredients } from "./tags.js";
-
-// export function mainSearch(search) {
-//   // console.log(typeof search);
-//   if (search === undefined || search.length < 3) {
-//     return allRecipes;
-//   } else {
-//     let searchResults = allRecipes.filter(
-//       (recipe) =>
-//         recipe.description.toLowerCase().includes(search) ||
-//         recipe.name.toLowerCase().includes(search) ||
-//         recipe.ingredients.some((ingredient) =>
-//           ingredient.ingredient.toLowerCase().includes(search)
-//         )
-//     );
-//     console.log(searchResults);
-//     console.log(searchResults.length);
-
-//     return searchResults;
-//   }
-// }
 
 export function mainSearch(search) {
   if (search === undefined || search.length < 3) {
